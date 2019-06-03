61fbb0998b51580a33e132767136622f10df4752
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2019-06-03 19:26:06 -0300

    add log sistem on tracking route'

--- app/controllers/tracking.js
+++ app/controllers/tracking.js
@@ -1,6 +1,7 @@
 "use strict"
 
 class TrackingController {
+  
   constructor () {
     this.API_KEY = "fcdcd1e2524d5ece4627f013eb428ed0209f4cec"
     this.API_BASE_URL = "https://app.readytrack.com.au/api/v1/"
@@ -8,8 +9,15 @@ class TrackingController {
   }
 
   async add(req, res, next) {
+    let fs = require('fs')
+
     let data = req.body // this is the data received from GPS when the car enter on the territory, should be saved on a database
-    console.log(data)
+    
+    let fileName = Date.now();
+    let filePath = __dirname + `/../../logs/${fileName}.txt`
+    fs.appendFileSync(filePath, JSON.stringify(data));
+
+    return res.status(200).send()
   }
 
 }
